002f34fbe2282cfdf83940a226ba3860da0588ad
romelmahmud <romelmahmud@outlook.com>
2021-02-16 00:09:09 +0600

    menu icon change when click

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,15 @@
 const burgerMenu = document.querySelector(".nav__burgerMenu");
 const dropDownMenu = document.querySelector(".dropDown__linksContainer");
+const menuIcon = document.querySelector(".nav__burgerMenu img");
 
 burgerMenu.addEventListener("click", () => {
   if (dropDownMenu.classList.contains("slide")) {
     dropDownMenu.classList.remove("slide");
     dropDownMenu.classList.remove("no-scroll");
+    menuIcon.setAttribute("src", "/image/icon-hamburger.svg");
   } else {
     dropDownMenu.classList.add("slide");
     dropDownMenu.classList.add("no-scroll");
+    menuIcon.setAttribute("src", "/image/icon-close.svg");
   }
 });
