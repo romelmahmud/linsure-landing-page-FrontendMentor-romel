f0c17984a0f1d9d8984cc8ef871be35528302e51
romelmahmud <romelmahmud@outlook.com>
2021-02-16 01:03:24 +0600

    complete vr 1.0

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,33 @@
+//? selecting element from DOM
+
 const burgerMenu = document.querySelector(".nav__burgerMenu");
 const dropDownMenu = document.querySelector(".dropDown__linksContainer");
 const menuIcon = document.querySelector(".nav__burgerMenu img");
+const body = document.querySelector("body");
 
-burgerMenu.addEventListener("click", () => {
+//?crreating toggle function
+
+const toggle = () => {
   if (dropDownMenu.classList.contains("slide")) {
-    dropDownMenu.classList.remove("slide");
-    dropDownMenu.classList.remove("no-scroll");
     menuIcon.setAttribute("src", "/image/icon-hamburger.svg");
+    dropDownMenu.classList.remove("slide");
+
+    body.classList.remove("no-scroll");
   } else {
     dropDownMenu.classList.add("slide");
-    dropDownMenu.classList.add("no-scroll");
+
     menuIcon.setAttribute("src", "/image/icon-close.svg");
+    body.classList.add("no-scroll");
   }
-});
+};
+
+//? toggle efect on Burger Menu icon and slide menu
+
+burgerMenu.addEventListener("click", () => toggle());
+
+//? disable silde dropdwon menu and no scrolling when mobile link clicked
+
+const navLinks = document.querySelectorAll(".mb-nav-links");
+const navLinkList = Array.from(navLinks);
+
+navLinkList.map((link) => link.addEventListener("click", () => toggle()));
